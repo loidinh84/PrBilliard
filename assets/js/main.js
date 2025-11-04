f3cce19383671005047a3e5a6fa525d227b82f3e
Thành Lợi <dinhhoangloibt@gmail.com>
2025-11-05 05:27:49 +0700

    Hoàn thành trang 1

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// const loginButton = document.querySelector(".header__action--login");
+// const signupButton = document.querySelector(".header__action--signup");
+// const navbarButton = document.querySelector(".navbar__link");
+
+// loginButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+// });
+// signupButtonButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+// });
+// navbarButtonButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+// });
